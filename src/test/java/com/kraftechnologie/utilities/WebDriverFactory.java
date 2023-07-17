a6857d805f308fd4f2146d92ad0e615c4aabaeae
eywas <umiteywas@gmail.com>
2023-07-17 20:48:58 +0300

    17 temmuz 2023

--- src/test/java/com/kraftechnologie/utilities/WebDriverFactory.java
+++ src/test/java/com/kraftechnologie/utilities/WebDriverFactory.java
@@ -3,9 +3,10 @@ package com.kraftechnologie.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class WebDriverFactory {
+
     //write a static method
     //that is named getDriver
     //it takes a string parameter (browserType)
@@ -14,19 +15,18 @@ public class WebDriverFactory {
 
     public static WebDriver getDriver(String browserType){
         WebDriver driver = null;
-        switch(browserType.toLowerCase()){
+        switch (browserType.toLowerCase()){
             case "chrome":
                 WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
-                driver.manage().window().maximize();
+                //driver.manage().window().maximize();
                 break;
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-                driver.manage().window().maximize();
+            case "safari":
+                WebDriverManager.safaridriver().setup();
+                driver = new SafariDriver();
+                //driver.manage().window().maximize();
                 break;
         }
         return driver;
     }
-
 }
